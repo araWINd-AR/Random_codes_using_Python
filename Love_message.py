ea7849fc32fb9bff6131e5ce54ddb2687c3482b4
Aravind Ganipisetty <aravindganipisetty@gmail.com>
2026-01-15 19:08:51 -0500

    Add files via upload

---
+++ Love_message.py
@@ -0,0 +1,52 @@
+import turtle
+import math
+
+def draw_heart_with_message(scale, message):
+
+    screen = turtle.Screen()
+    heart_turtle = turtle.Turtle()
+    heart_turtle.pencolor('red')
+    heart_turtle.fillcolor('red')
+    heart_turtle.pensize(2)
+    heart_turtle.speed(0) 
+    heart_turtle.penup()
+    heart_turtle.goto(0, -scale * 2.5) 
+    heart_turtle.pendown()
+    heart_turtle.begin_fill()
+    
+    
+    for t in range(0, 628):
+        t = t / 100.0 
+
+       
+        x = scale * 16 * math.sin(t)**3
+        y = scale * (13 * math.cos(t) - 5 * math.cos(2 * t) - 2 * math.cos(3 * t) - math.cos(4 * t))
+
+        heart_turtle.goto(x, y) 
+
+    heart_turtle.end_fill()
+    
+    
+    heart_turtle.penup() 
+   
+    heart_turtle.goto(0, -scale * 1.5) 
+    
+    
+    heart_turtle.color("white")
+    
+  
+    heart_turtle.write(
+        message, 
+        align="center", 
+        font=("Georgia", int(scale * 1.8), "bold") # Font size scaled to the heart
+    )
+
+    
+    heart_turtle.hideturtle()
+    screen.mainloop()
+
+
+draw_heart_with_message(
+    scale=10, 
+    message="I love you "
+)
